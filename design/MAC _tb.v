f3b056850d34c2f0dd0cda7cf043d0b8140aa2ed
G NAYANESH REDDY <84563214+nayanesh-reddy@users.noreply.github.com>
2022-10-30 13:44:04 +0530

    Update MAC _tb.v

--- design/MAC _tb.v
+++ design/MAC _tb.v
@@ -1,34 +1,36 @@
 module MAC_tb();
-  
-  reg [7:0] a,b;
+
+  reg [15:0] x,w;
   reg clk = 0;
   reg rst = 0;
-  
-  wire [15:0] out;
-  
-  MAC dut( .a(a), .b(b), .clk(clk), .rst(rst), .out(out) );
-  
+
+  wire [31:0] out;
+
+  MAC dut( .x(x), .w(w), .clk(clk), .rst(rst), .out(out) );
+
+  defparam dut.N = 8;
+
   always #5 clk = ~clk;
-  
+
   initial
     begin
       #2; rst = 1;
       #5; rst = 0;
-      
-      #0; a = 8'd5; b = 8'd2;
-      #10; a = 8'd3; b = 8'd4;
-      #10; a = 8'd7; b = 8'd9;
-      #10; a = 8'd8; b = 8'd6;
-      #10; a = 8'd0; b = 8'd0;
-      
+
+      #0;  x = 8'd5; w = 8'd2;
+      #10; x = 8'd3; w = 8'd4;
+      #10; x = 8'd7; w = 8'd9;
+      #10; x = 8'd8; w = 8'd6;
+      #10; x = 8'd0; w = 8'd0;
+
       #10 $finish();
     end
-  
+
   initial
     begin
-      $monitor("clk = %b, a = %d; b = %d, out = %d", clk, a, b, out );
+      $monitor("clk = %b, x = %d; w = %d, out = %d", clk, x, w, out );
       $dumpfile("dump.vcd");
       $dumpvars;
     end
-  
+
 endmodule
